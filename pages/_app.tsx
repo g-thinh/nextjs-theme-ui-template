9978ccfdc30d514ff8b52a812a90c7965d5accef
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-04-25 17:13:24 -0400

    added theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app';
 import GlobalStyles from '@styles/GlobalStyles';
+import { theme } from '@styles/theme';
+import { ThemeProvider } from 'theme-ui';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles styles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
