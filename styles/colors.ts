6386f5286c75bf4c765bc48065f0b029d6b0aeec
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-04-25 17:12:54 -0400

    updated themes

---
+++ styles/colors.ts
@@ -0,0 +1,20 @@
+const colors = {
+  text: '#000',
+  background: '#fff',
+  primary: '#07c',
+  secondary: '#30c',
+  muted: '#f6f6f6',
+  modes: {
+    dark: {
+      text: 'hsl(210, 50%, 96%)',
+      background: 'hsl(230, 25%, 18%)',
+      primary: 'hsl(260, 100%, 80%)',
+      secondary: 'hsl(290, 100%, 80%)',
+      highlight: 'hsl(260, 20%, 40%)',
+      purple: 'hsl(290, 100%, 80%)',
+      muted: 'hsla(230, 20%, 0%, 20%)',
+      gray: 'hsl(210, 50%, 60%)',
+    },
+  },
+};
+export default colors;
